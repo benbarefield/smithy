9e549d15d432e8b88437c5a19b331055cdfe0c1a
Ben Barefield <benbarefield@gmail.com>
2018-11-17 16:56:57 -0800

    updating content and styling of gameTime"

--- src/GameTime/View.js
+++ src/GameTime/View.js
@@ -5,13 +5,7 @@ import React from 'react'
 class GameTime extends React.Component {
     render() {
         return (
-            <div className="container">
-                <div className={"time--container"}>
-                    Time: {this.props.time}
-                </div>
-                <div className={'key--container'}>
-                    Key: {this.props.keyCode}
-                </div>
+            <div className="game-time--container">
                 <button className={this.props.paused ? 'selected' : ''} onClick={e => {if(e.detail > 0) {this.props.sinks.pauseButton(true)}}}>
                     Pause
                 </button>
@@ -24,3 +18,12 @@ class GameTime extends React.Component {
 }
 
 export default GameTime;
+
+/*
+<div className={"time--container"}>
+                    Time: {this.props.time}
+                </div>
+                <div className={'key--container'}>
+                    Key: {this.props.keyCode}
+                </div>
+ */
