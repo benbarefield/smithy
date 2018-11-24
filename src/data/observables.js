f57940c85527d0ed6e11d40e46ca00c53168f50c
Ben Barefield <ben@smartsparrow.com>
2018-11-24 11:36:11 -0800

    seasons changes

--- src/data/observables.js
+++ src/data/observables.js
@@ -10,11 +10,11 @@ export let whichKeyUp = (subjects, observables) =>
 const SPACE_BAR = 32;
 export let paused = (subjects, observables) =>
     merge(observables.whichKeyUp, subjects.pauseButton)
-        .pipe(scan((paused, value) => {
-            if(value === true) { return true; }
-            if(value === false) { return false; }
-            return value === SPACE_BAR ? !paused : paused
-        }, false));
+    .pipe(scan((paused, value) => {
+        if(value === true) { return true; }
+        if(value === false) { return false; }
+        return value === SPACE_BAR ? !paused : paused
+    }, false));
 
 // required observables: paused
 export let timeData = (subjects, observables) =>
@@ -26,12 +26,25 @@ export let timeData = (subjects, observables) =>
         };
     }, { elapsed: 0, total: 0 }));
 
+function nextSeason(currentSeason) {
+    switch(currentSeason) {
+        case 'spring': return 'summer';
+        case 'summer': return 'autumn';
+        case 'autumn': return 'winter';
+        case 'winter': return 'spring';
+    }
+}
 export let season = (subjects, observables) =>
     observables.timeData
-        .pipe(scan((data, time) =>
-            Object.assign({}, data, {
-                time: data.time - time.elapsed
-        }), {
-            time: 60 * 1000,
-            current: 'summer'
-        }));
+    .pipe(scan((data, time) => {
+        let remaining = data.time - time.elapsed;
+        let currentSeason = data.current;
+        if(remaining <= 0) {
+            remaining = 60 * 1000;
+            currentSeason = nextSeason(currentSeason);
+        }
+        return Object.assign({}, data, {time : remaining, current: currentSeason})
+    }, {
+        time: 60 * 1000,
+        current: 'spring'
+    }));
