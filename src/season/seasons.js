28d0b27d1aa88079f3c45fd6bc43940bbfec8a7b
Ben Barefield <ben@smartsparrow.com>
2018-11-28 23:50:02 -0800

    determining when to do jobs

--- src/season/seasons.js
+++ src/season/seasons.js
@@ -11,9 +11,17 @@ Object.keys(seasons).forEach(k => Object.freeze(seasons[k]));
 export let SEASONS = seasons;
 
 export function nextSeason(currentSeasonName) {
+    let nextSeasonInfo = followingSeason(currentSeasonName);
+    let jobTimes = new Array(determineNumberOfJobs(nextSeasonInfo));
+    let sectionLength = Math.floor(60 * 1000 / jobTimes.length);
+    for(let i = 0; i < jobTimes.length: ++i) {
+        jobTimes[i] = (Math.random() * sectionLength) + (i * sectionLength);
+    }
     return {
         time: 60 * 1000,
-        info: followingSeason(currentSeasonName)
+        info: nextSeasonInfo,
+        jobGenerator: jobGeneratorMap[nextSeasonInfo.name],
+        jobTimes
     };
 }
 function followingSeason(currentSeasonName) {
@@ -23,4 +31,26 @@ function followingSeason(currentSeasonName) {
         case seasons.AUTUMN.name: return seasons.WINTER;
         default: return seasons.SPRING;
     }
+}
+function determineNumberOfJobs(season) {
+    return Math.random() < 0.25 ? 1 : 0;
+}
+
+const jobGeneratorMap = {
+    [seasons.SPRING.name]: springJobGenerator,
+    [seasons.SUMMER.name]: summerJobGenerator,
+    [seasons.AUTUMN.name]: autumnJobGenerator,
+    [seasons.WINTER.name]: winterJobGenerator
+};
+function springJobGenerator(statusEffects) {
+    return null;
+}
+function summerJobGenerator(statusEffects) {
+    return null;
+}
+function autumnJobGenerator(statusEffects) {
+    return null;
+}
+function winterJobGenerator(statusEffects) {
+    return null;
 }
